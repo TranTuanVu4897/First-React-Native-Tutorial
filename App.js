852a3c27a04ca65849d1b0449d9b4cc6f2433dd4
Tran Vu <vu.trantuan.1997@gmail.com>
2021-03-13 15:12:05 +0700

    First app

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.boldText}>Hello</Text>
+            </View>
+            <View style={styles.body}>
+                <Text style={styles.boldText}>
+                    lorem <Text>ipsum</Text> dolor sit amet.
+                </Text>
+                <Text>lorem ipsum dolor sit amet.</Text>
+                <Text>lorem ipsum dolor sit amet.</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        backgroundColor: 'pink',
+        padding: 20,
+    },
+    boldText: {
+        fontWeight: 'bold',
+    },
+    body: {
+        backgroundColor: 'yellow',
+        padding: 20,
+        fontWeight: 'bold',
+    },
 });
