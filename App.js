0947032551a194a14ff0af7655a2df24f17f2800
Tran Vu <vu.trantuan.1997@gmail.com>
2021-03-14 18:54:29 +0700

    Flat list

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; //this use hook
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+    const [people, setPeople] = useState([
+        { name: 'John', id: '1' },
+        { name: 'Mike', id: '2' },
+        { name: 'Tome', id: '3' },
+        { name: 'Mart', id: '4' },
+        { name: 'Anna', id: '5' },
+        { name: 'Bobs', id: '6' },
+        { name: 'Lizt', id: '7' },
+        { name: 'Nine', id: '8' },
+        { name: 'Trav', id: '9' },
+        { name: 'Cerv', id: '10' },
+        { name: 'Dodg', id: '11' },
+        { name: 'Illi', id: '12' },
+    ]);
+
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.boldText}>Hello</Text>
-            </View>
-            <View style={styles.body}>
-                <Text style={styles.boldText}>
-                    lorem <Text>ipsum</Text> dolor sit amet.
-                </Text>
-                <Text>lorem ipsum dolor sit amet.</Text>
-                <Text>lorem ipsum dolor sit amet.</Text>
-            </View>
+            {/* less code than scroll view, performance better? */}
+            <FlatList
+                numColumns={2}
+                keyExtractor={item => item.id}
+                data={people}
+                renderItem={({ item }) => (
+                    <Text style={styles.item}>{item.name}</Text>
+                )}
+            />
+            {/* <ScrollView>
+                {people.map(i => (
+                    <View key={i.key}>
+                        <Text style={styles.item}>{i.name}</Text>
+                    </View>
+                ))}
+            </ScrollView> */}
         </View>
     );
 }
@@ -22,19 +43,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingTop: 40,
+        paddingHorizontal: 20,
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
-    header: {
-        backgroundColor: 'pink',
-        padding: 20,
-    },
-    boldText: {
-        fontWeight: 'bold',
-    },
-    body: {
-        backgroundColor: 'yellow',
-        padding: 20,
-        fontWeight: 'bold',
+    item: {
+        marginTop: 24,
+        padding: 30,
+        backgroundColor: 'aqua',
+        fontSize: 24,
     },
 });
